f0909a20489756ce2371155f1dde2e751e020d33
Jorge Bolaños <jorgedbolanosgh@gmail.com>
2024-11-05 17:51:32 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Types of Pasta!</h1>
+    <ul>
+      <li>Bolognese</li>
+      <li>Pesto</li>
+      <li>Alfredo</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
